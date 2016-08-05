505a5b129712870ce1d33da0f88f280adefbf94e
Anastasia <anastasia.aizman@gmail.com>
2016-08-05 11:46:28 -0400

    update - ngram file

--- word_usage_ngrams.py
+++ word_usage_ngrams.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python
 import io
 import os
+import re
 import sys
+import json
+import logging
 import unicodedata
+from datetime import datetime
 from pyspark.sql.types import *
 from pyspark.sql.utils import *
 from pyspark.sql import SQLContext
 from pyspark import SparkContext
+from collections import Counter
+from pyspark.sql.types import Row
+
 sc = SparkContext("local", "Simple App")
 sqlContext = SQLContext(sc)
-
 reload(sys)
 sys.setdefaultencoding("utf-8")
-import json
-import re
-import logging
-import datetime
-global date
-global state
-global word_dict
-global region_path
+timestamp = re.sub(r'[\s\\:.+]', '_', str(datetime.today()))
 
-log_file = "%s/word_usage_2.log" % os.path.curdir
-error_file = "%s/error_file.txt" % os.path.curdir
-word_ngrams_dir = "%s/word_ngrams_output/" % os.path.curdir
-logging.basicConfig(format='%(levelname)s:%(message)s', filename=log_file,  level=logging.DEBUG)
+log_file = "%s/word_usage_%s.log" % (os.path.curdir, timestamp)
+logging.basicConfig(format='%(levelname)s:%(message)s', filename=log_file, level=logging.ERROR)
+error_file = "%s/error_file_%s.txt" % (os.path.curdir, timestamp)
+word_ngrams_dir = "%s/words_%s/" % (os.path.curdir, timestamp)
+
 exclude_states = []
-word_dict = {}
 
 if not os.path.exists(word_ngrams_dir):
     os.makedirs(word_ngrams_dir)
@@ -48,75 +47,65 @@ stopwords = [
     'not','with','you','which',
     'are', 'then', 'them',
     'also','that', 'its', 'was', 'this',
-    'that', 'and', 'the', 'has', 'can',
-    ]
+    'that', 'and', 'the', 'has', 'can']
+
 alphabet_words = re.compile('[^a-zA-Z]')
 
-def split_string(s):
+def break_up_words(s):
     s = s.lower().split(' ')
-    result = map(filter_word, s)
-    logging.info('WORD_USAGE_LOG split_string result %s', result)
-    return result
+    mapped = map(fix_word, s)
+    return filter(filter_word, mapped)
 
-def filter_word(word):
-    logging.info('filter_word: %s', word)
-    if word:
-        try:
-            word = word.encode('utf8')
-        except Exception as e:
-            logging.warning('WORD_USAGE_LOG filter_word UnicodeDecodeError: %s %s', word, e)
-            pass
-
-        word = alphabet_words.sub('', word)
-        if (word not in stopwords) and (len(word) > 2):
-            logging.info('filter_word returning %s', word)
-            return word
-
-def get_decisiondate(x):
-    global date
-    date_string = x.select('decisiondate').collect()[0][0]['#VALUE']
-    possible_dates = re.findall(r'\d+',date_string)
-    now = datetime.datetime.now()
-    for d in possible_dates:
-        try:
-            d = int(d)
-            if d > 1500 and d < now.year + 2:
-                date = d
-                return date
-        except:
-            logger.warning('possible date error %s', single_file)
-            pass
-    raise Exception('Date not found')
-
-def parse_elements(x):
-    word_list = []
+def fix_word(word):
     try:
-        if type(x) is int:
-            return
-        for element in x:
-            logging.info('WORD_USAGE_LOG parse_elements: element %s type %s', element, type(element))
-            if not element: continue
-            if (type(element) is list):
-                for n in element:
-                    word_list.append(parse_elements(n))
-            elif (type(element) is str):
-                word_list.append(split_string(element))
-            elif type(element) is unicode:
-                element = unicodedata.normalize('NFKD', element).encode('ascii','ignore')
-                word_list.append(split_string(element))
-            elif type(element) is int:
-                continue
-            else:
-                val = extract_row_value(element)
-                word_list = word_list + split_string(val)
+        word = word.encode('utf8')
     except Exception as e:
         pass
-    return word_list
 
-def extract_row_value(row):
-    return row['#VALUE']
+    return alphabet_words.sub('', word)
 
+def filter_word(word):
+    return (word not in stopwords) and (len(word) > 2)
 
+def get_decisiondate(x):
+    now = datetime.now().year
+    if type(x) is int:
+        return x
+    else:
+        try:
+            possible_dates = x.split('-')
+            d = [int(x) for x in possible_dates if x.isdigit() and len(x) == 4 and int(x) > 1500 and int(x) < now + 4 ]
+            return d[0]
+        except:
+            raise Exception('Date not found')
+
+def clean_string(val):
+    if type(val) is unicode:
+        return unicodedata.normalize('NFKD', val).encode('ascii','ignore')
+    else:
+        return val
+
+def parse_elements(x):
+    the_words = []
+    try:
+        if type(x) is Row:
+            return parse_elements(x.asDict(recursive=True))
+        elif type(x) is list:
+            for n in x:
+                the_words += parse_elements(n)
+            return the_words
+        elif type(x) is dict:
+            for key,element in x.items():
+                the_words += parse_elements(clean_string(element))
+            return the_words
+        elif type(x) is str:
+            return break_up_words(x)
+        elif type(x) is unicode:
+            return break_up_words(clean_string(x))
+        else:
+            return []
+    except Exception as e:
+        return []
 
 class NestedReadCreate(object):
      def __init__(self, root):
@@ -142,15 +131,12 @@ class NestedReadCreate(object):
 
             return obj
 
-def write_words_to_json_files(file_path):
-    global date
-    global state
-    global word_dict
-    global region_path
+def write_words_to_json_files(word_dict, date):
+    global state, region_path
 
-    for word in word_dict.iterkeys():
+    for word, count in word_dict.items():
         word_filename = "%s%s.txt" % (word_ngrams_dir, word)
-        local_word_count = word_dict[word]
+        count = word_dict[word]
         date = str(date)
         full_region_path = ('.').join([x for x in region_path])
         with touch_open(word_filename, 'r+') as f:
@@ -158,22 +144,21 @@ def write_words_to_json_files(file_path):
                 data = json.load(f)
             except Exception as e:
                 data = f.read()
+                logging.warning('write_words_to_json_files: %s', e)
                 if not data:
                     clear_file(f)
                     data = {}
                 pass
+
             nrc = NestedReadCreate(data)
             # checking if properties exist, creating them if they don't
-            nrc(lookups=[full_region_path, date], count=local_word_count)
-            nrc(lookups=[state, 'total_state', date], count=local_word_count)
-            nrc(lookups=['total_country', date], count=local_word_count)
+            nrc(lookups=[full_region_path, date], count=count)
+            nrc(lookups=[state, 'total_state', date], count=count)
+            nrc(lookups=['total_country', date], count=count)
+
             clear_file(f)
             json.dump(data, f)
     return
-
-def get_local_word_counts(word):
-    global word_dict
-    word_dict[word] = word_dict[word] + 1 if word_dict.get(word) else 1
 
 def clear_file(f):
     f.seek(0)
@@ -192,17 +177,12 @@ def parse_path(path):
     path_list = path.split('/')
     for (idx,s) in enumerate(path_list):
         if s in state_list:
-            state = s
             # keep everything from path except last thing which is the .xml
             path = path_list[idx:]
             path.pop()
-            return [state, path]
+            return [s, path]
     # if state not here, take a guess
     return [path_list[3], path_list[4:]]
-
-def prepare_word_dictionary():
-    global word_dict
-    word_dict = {}
 
 def is_xml(file):
     return file[-4:] == '.xml'
@@ -211,47 +191,34 @@ def is_file(file):
     # TODO: how does this happen?!
     return os.path.isfile(file)
 
-def walk_through_words(words):
-    for word in words:
-        if type(word) is list:
-            walk_through_words(word)
-        else:
-            get_local_word_counts(word)
-
 def get_word_usage(files_array=[]):
-    prepare_word_dictionary()
-
     global state
-
+    word_list = []
     for single_file in files_array:
-        if not is_xml(single_file) or not is_file(single_file):
-            continue
-
-        logging.info('parsing file: %s', single_file)
-
-        df = sqlContext.read.format('com.databricks.spark.xml').options(rowTag='casebody').load(single_file)
-        cols = get_columns(df.columns)
-
+        if not is_xml(single_file) or not is_file(single_file): continue
         try:
-            date = get_decisiondate(df)
-            words = df.select(*(i for i in cols)).map(lambda x: x[0] if not type(x[0]) is int else []).map(lambda x: parse_elements(x)).reduce(lambda l: [x for x in l if x is not None])
-            walk_through_words(words)
+            date = sqlContext.read.format('com.databricks.spark.xml').options(rowTag='case').load(single_file).select('decisiondate').map(lambda x: get_decisiondate(x.decisiondate)).collect()[0]
 
+            df = sqlContext.read.format('com.databricks.spark.xml').options(rowTag='xmlData').load(single_file)
+            cols = get_columns(df.columns)
+
+            columns = df.select(*(i for i in cols))
+            parsed_dicts = columns.map(lambda x: x.asDict(recursive=True))
+            parsed_words = parsed_dicts.flatMap(lambda x: [parse_elements(x[m]) for m in x])
+            words = parsed_words.flatMap(lambda x: x).collect()
         except Exception as e:
-            logging.error('Error: %s, file: %s', e, single_file)
-            with open(error_file, 'w') as ef:
-                ef.write('%s\n' % single_file)
+            logging.error('get_word_usage: %s %s', single_file, e)
+            print "exception:",e
             continue
 
-        write_words_to_json_files(file_path=single_file)
+        word_dict = Counter(words)
+        write_words_to_json_files(word_dict, date)
 
 def get_words_for_all_in_dir():
     current_dir = sys.argv[1]
     for root, dirs, files in os.walk(current_dir):
         if len(files):
-            global state
-            global region_path
-
+            global state, region_path
             files = [os.path.join(root, f) for f in files if f != '.DS_Store']
             try:
                 state, region_path = parse_path(files[0])
@@ -260,9 +227,7 @@ def get_words_for_all_in_dir():
 
             if state in exclude_states: continue
 
-            logging.info("WORD_USAGE_LOG %s", state)
-            get_word_usage(files_array=files)
-
+            word_list = get_word_usage(files_array=files)
 
 if __name__ == "__main__":
     get_words_for_all_in_dir()
